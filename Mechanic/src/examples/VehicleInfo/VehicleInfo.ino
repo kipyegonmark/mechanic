8fc9a14b3446ed0cb56ce5bed5ba0ea69aa6f784
joerg.pleumann <joerg.pleumann@gmail.com>
2013-05-05 08:07:42 +0000

    Import of current working state, derived from Railuino. Still needs a lot of cleanup.

---
+++ Mechanic/src/examples/VehicleInfo/VehicleInfo.ino
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * Mechanic - On-board diagnosis using Arduino and CAN
+ *
+ * Copyright (C) 2013 Joerg Pleumann
+ * 
+ * This example is free software; you can redistribute it and/or
+ * modify it under the terms of the Creative Commons Zero License,
+ * version 1.0, as published by the Creative Commons Organisation.
+ * This effectively puts the file into the public domain.
+ *
+ * This example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LICENSE file for more details.
+ */
+
+// IWF0DXXGAJD5B65838
+
+#include <Mechanic.h>
+
+ObdInterface obd;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  
+  obd.setSlow(false);
+  obd.setExtended(false);
+  obd.setDebug(true);
+  obd.begin();
+}
+
+void loop() {
+  char buffer[64];
+  int count = 0;
+  
+  if (obd.getMultiframePid(0x09, 0x02, buffer, count)) {
+    Serial.print("Count: ");
+    Serial.println(count, DEC);
+    
+    buffer[count] = 0;
+    
+    Serial.println(buffer);
+  }
+
+  for(;;);
+}
+
