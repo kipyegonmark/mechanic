c0a6266b7110cc4b9cf5d283892d517b9c389648
joerg.pleumann@gmail.com <joerg.pleumann@gmail.com@c98d014b-1cdf-036b-b6a9-0b9b4bd1df11>
2013-05-21 15:04:23 +0000

    Updating all license headers

--- trunk/MechanicApp/src/de/pleumann/android/mechanic/BluetoothHelper.java
+++ trunk/MechanicApp/src/de/pleumann/android/mechanic/BluetoothHelper.java
@@ -1,3 +1,18 @@
+/*********************************************************************
+ * Mechanic - Hacking your car
+ *
+ * Copyright (C) 2013 Joerg Pleumann
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LICENSE file for more details.
+ */
 package de.pleumann.android.mechanic;
 
 import java.io.IOException;
