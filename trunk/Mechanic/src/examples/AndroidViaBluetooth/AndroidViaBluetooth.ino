b1a359ecd474d7d28d94e530a7bdd1d8f3688b4e
joerg.pleumann@gmail.com <joerg.pleumann@gmail.com@c98d014b-1cdf-036b-b6a9-0b9b4bd1df11>
2013-10-06 18:56:49 +0000

    Bluetooth sketch now able to deliver vehicle ID and trouble codes on request

--- trunk/Mechanic/src/examples/AndroidViaBluetooth/AndroidViaBluetooth.ino
+++ trunk/Mechanic/src/examples/AndroidViaBluetooth/AndroidViaBluetooth.ino
@@ -26,6 +26,8 @@ boolean hasSpeed = false, hasRpm = false, hasLoad = false, hasTemp = false, hasF
 
 float speed = 0, rpm = 0, load = 0, temp = 0, fuel = 0;
 
+char buffer[256];
+
 boolean probe(boolean slow, boolean extended) {
   boolean dummy;
   
@@ -67,7 +69,65 @@ void setup() {
   obd.isPidSupported(0x2f, hasFuel);
 }
 
+void receiveRequest() {
+  int length = 0;
+  boolean newline = false;
+
+  while (!newline && length < sizeof(buffer)) {
+    if (blue.available() > 0) {
+      int c = blue.read();
+      
+      if (c == 10) {
+        buffer[length++] = 0;
+        newline = true;
+      } else if (c == 13) {
+        // Ignore
+      } else {
+        buffer[length++] = c;
+      }
+    }
+  }
+}
+
 void loop() {
+  if (blue.available() > 0) {
+	receiveRequest();
+	
+	if (strcmp("vin", request)) {
+      blue.print("vin,");
+	  if (obd.getMultiframePid(0x09, 0x02, buffer, count)) {
+		buffer[count] = 0;
+        blue.print(buffer);
+	  }
+      blue.println();
+	} else if (strcmp("dtc", request)) {
+	  if (obd.getMultiframePid(0x03, -1, buffer, count)) {
+		blue.print("dtc");
+		for (int i = 0; i < count; i += 2) {
+	      blue.print(",");
+	      
+		  byte first = (byte)buffer[i];
+		  byte second = (byte)buffer[i + 1];
+		  
+		  switch(first >> 6) {
+			case 0: blue.print("P"); break;
+			case 1: blue.print("C"); break;
+			case 2: blue.print("B"); break;
+			case 3: blue.print("U"); break;
+		  }
+		 
+		  blue.print((first >> 4) & 0x03, DEC);
+		  
+		  blue.print(first & 0x0f, HEX);
+		  blue.print(second >> 4, HEX);
+		  blue.print(second & 0x0f, HEX);
+		}
+		
+		blue.println();
+	  }
+	}
+  }
+  
   if (hasSpeed) {
     obd.getPidAsFloat(0x0d, 0.0f, 255.0f, speed);
   }
@@ -88,6 +148,7 @@ void loop() {
 	obd.getPidAsFloat(0x2F, 0.0f, 100.0f, fuel);
   }
 
+  blue.print("mom,");
   blue.print(obd.isSlow() ? "true," : "false,");
   blue.print(obd.isExtended() ? "true," : "false,");
   blue.print(speed);
